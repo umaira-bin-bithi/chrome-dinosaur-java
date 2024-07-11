def5f616303d3528fd7e4dcc0057dd0d6b3c7c9d
umaira-bin-bithi <umairabinbithi@gmail.com>
2024-07-11 14:07:56 +0600

    final work

--- ChromeDinosaur.java
+++ ChromeDinosaur.java
@@ -1,5 +1,6 @@
 
-import java.awt.*;
+
+    import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
@@ -8,59 +9,15 @@ public class ChromeDinosaur extends JPanel implements ActionListener, KeyListene
     int boardWidth = 750;
     int boardHeight = 250;
 
-    // images
-    Image dinosaurImg;
-    Image dinosaurDeadImg;
-    Image dinosaurJumpImg;
-    Image cactus1Img;
-    Image cactus2Img;
-    Image cactus3Img;
+    private Dinosaur dinosaur;
+    private ArrayList<Cactus> cactusArray;
 
-    class Block {
-        int x;
-        int y;
-        int width;
-        int height;
-        Image img;
+    private boolean gameOver = false;
+    private boolean gameStarted = false;
+    private int score = 0;
 
-        Block(int x, int y, int width, int height, Image img) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.img = img;
-        }
-    }
-
-    // dinosaur
-    int dinosaurWidth = 88;
-    int dinosaurHeight = 94;
-    int dinosaurX = 50;
-    int dinosaurY = boardHeight - dinosaurHeight;
-
-    Block dinosaur;
-
-    // cactus
-    int cactus1Width = 34;
-    int cactus2Width = 69;
-    int cactus3Width = 102;
-
-    int cactusHeight = 70;
-    int cactusX = 700;
-    int cactusY = boardHeight - cactusHeight;
-    ArrayList<Block> cactusArray;
-
-    // physics
-    int velocityX = -12; // cactus moving left speed
-    int velocityY = 0; // dinosaur jump speed
-    int gravity = 1;
-
-    boolean gameOver = false;
-    boolean gameStarted = false;
-    int score = 0;
-
-    Timer gameLoop;
-    Timer placeCactusTimer;
+    private Timer gameLoop;
+    private Timer placeCactusTimer;
 
     public ChromeDinosaur() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -68,104 +25,73 @@ public class ChromeDinosaur extends JPanel implements ActionListener, KeyListene
         setFocusable(true);
         addKeyListener(this);
 
-        dinosaurImg = new ImageIcon(getClass().getResource("./img/dino-run.gif")).getImage();
-        dinosaurDeadImg = new ImageIcon(getClass().getResource("./img/dino-dead.png")).getImage();
-        dinosaurJumpImg = new ImageIcon(getClass().getResource("./img/dino-jump.png")).getImage();
-        cactus1Img = new ImageIcon(getClass().getResource("./img/cactus1.png")).getImage();
-        cactus2Img = new ImageIcon(getClass().getResource("./img/cactus2.png")).getImage();
-        cactus3Img = new ImageIcon(getClass().getResource("./img/cactus3.png")).getImage();
+        // Initialize dinosaur
+        dinosaur = new Dinosaur();
 
-        // dinosaur
-        dinosaur = new Block(dinosaurX, dinosaurY, dinosaurWidth, dinosaurHeight, dinosaurImg);
-        // cactus
-        cactusArray = new ArrayList<Block>();
+        // Initialize cactus array
+        cactusArray = new ArrayList<>();
 
-        // game timer
-        gameLoop = new Timer(1000 / 60, this); // 1000/60 = 60 frames per 1000ms (1s), update
+        // Game timer
+        gameLoop = new Timer(1000 / 60, this); // 60 frames per second
 
-        // place cactus timer
-        placeCactusTimer = new Timer(1500, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeCactus();
-            }
-        });
+        // Place cactus timer
+        placeCactusTimer = new Timer(1500, e -> placeCactus());
     }
 
-    void placeCactus() {
-        if (gameOver) {
-            return;
-        }
+    private void placeCactus() {
+        if (gameOver) return;
 
         double placeCactusChance = Math.random(); // 0 - 0.999999
-        if (placeCactusChance > .90) { // 10% you get cactus3
-            Block cactus = new Block(cactusX, cactusY, cactus3Width, cactusHeight, cactus3Img);
-            cactusArray.add(cactus);
-        } else if (placeCactusChance > .70) { // 20% you get cactus2
-            Block cactus = new Block(cactusX, cactusY, cactus2Width, cactusHeight, cactus2Img);
-            cactusArray.add(cactus);
-        } else if (placeCactusChance > .50) { // 20% you get cactus1
-            Block cactus = new Block(cactusX, cactusY, cactus1Width, cactusHeight, cactus1Img);
-            cactusArray.add(cactus);
-        }
+        Cactus cactus = CactusFactory.createCactus(placeCactusChance, boardHeight);
+        cactusArray.add(cactus);
 
         if (cactusArray.size() > 10) {
             cactusArray.remove(0); // remove the first cactus from ArrayList
         }
     }
 
-    public void paintComponent(Graphics g) {
+    @Override
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
 
-    public void draw(Graphics g) {
-        // dinosaur
-        g.drawImage(dinosaur.img, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null);
+    private void draw(Graphics g) {
+        // Draw dinosaur
+        dinosaur.draw(g);
 
-        // cactus
-        for (int i = 0; i < cactusArray.size(); i++) {
-            Block cactus = cactusArray.get(i);
-            g.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height, null);
+        // Draw cactus
+        for (Cactus cactus : cactusArray) {
+            cactus.draw(g);
         }
 
-        // score
+        // Draw score
         g.setColor(Color.black);
         g.setFont(new Font("Courier", Font.PLAIN, 32));
         g.drawString(String.valueOf(score), 10, 35);
     }
 
-    public void move() {
-        if (!gameStarted || gameOver) {
-            return;
-        }
+    private void move() {
+        if (!gameStarted || gameOver) return;
 
-        // dinosaur
-        velocityY += gravity;
-        dinosaur.y += velocityY;
+        // Move dinosaur
+        dinosaur.move();
 
-        if (dinosaur.y > dinosaurY) { // stop the dinosaur from falling past the ground
-            dinosaur.y = dinosaurY;
-            velocityY = 0;
-            dinosaur.img = dinosaurImg;
-        }
+        // Move cactus and check for collisions
+        for (Cactus cactus : cactusArray) {
+            cactus.move();
 
-        // cactus
-        for (int i = 0; i < cactusArray.size(); i++) {
-            Block cactus = cactusArray.get(i);
-            cactus.x += velocityX;
-
-            if (collision(dinosaur, cactus)) {
+            if (dinosaur.collidesWith(cactus)) {
                 gameOver = true;
-                dinosaur.img = dinosaurDeadImg;
+                dinosaur.die();
                 placeCactusTimer.stop();
                 gameLoop.stop();
-                showEndDialog("Continue Again !", false);
+                showEndDialog("Continue Again ! ", false);
                 return;
             }
         }
 
-        // score
+        // Update score
         score++;
         if (score >= 1001) {
             gameOver = true;
@@ -173,13 +99,6 @@ public class ChromeDinosaur extends JPanel implements ActionListener, KeyListene
             gameLoop.stop();
             showEndDialog("***Winner***", true);
         }
-    }
-
-    boolean collision(Block a, Block b) {
-        return a.x < b.x + b.width &&   // a's top left corner doesn't reach b's top right corner
-               a.x + a.width > b.x &&   // a's top right corner passes b's top left corner
-               a.y < b.y + b.height &&  // a's top left corner doesn't reach b's bottom left corner
-               a.y + a.height > b.y;    // a's bottom left corner passes b's top left corner
     }
 
     @Override
@@ -197,9 +116,8 @@ public class ChromeDinosaur extends JPanel implements ActionListener, KeyListene
                 placeCactusTimer.start();
             }
 
-            if (dinosaur.y == dinosaurY && !gameOver) {
-                velocityY = -17;
-                dinosaur.img = dinosaurJumpImg;
+            if (dinosaur.isOnGround() && !gameOver) {
+                dinosaur.jump();
             }
 
             if (gameOver) {
@@ -217,9 +135,7 @@ public class ChromeDinosaur extends JPanel implements ActionListener, KeyListene
     public void keyReleased(KeyEvent e) {}
 
     private void resetGame() {
-        dinosaur.y = dinosaurY;
-        dinosaur.img = dinosaurImg;
-        velocityY = 0;
+        dinosaur.reset();
         cactusArray.clear();
         score = 0;
         gameOver = false;
@@ -249,3 +165,115 @@ public class ChromeDinosaur extends JPanel implements ActionListener, KeyListene
         frame.setVisible(true);
     }
 }
+
+class Block {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Image img;
+
+    public Block(int x, int y, int width, int height, Image img) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.img = img;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    public Image getImg() { return img; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setImg(Image img) { this.img = img; }
+
+    public void draw(Graphics g) {
+        g.drawImage(img, x, y, width, height, null);
+    }
+}
+
+class Dinosaur extends Block {
+    private static final int JUMP_VELOCITY = -17;
+    private static final int GRAVITY = 1;
+
+    private int velocityY = 0;
+    private int groundY;
+
+    private Image runImg;
+    private Image jumpImg;
+    private Image deadImg;
+
+    public Dinosaur() {
+        super(50, 156, 88, 94, new ImageIcon(Dinosaur.class.getResource("./img/dino-run.gif")).getImage());
+        runImg = getImg();
+        jumpImg = new ImageIcon(Dinosaur.class.getResource("./img/dino-jump.png")).getImage();
+        deadImg = new ImageIcon(Dinosaur.class.getResource("./img/dino-dead.png")).getImage();
+        groundY = getY();
+    }
+
+    public void move() {
+        velocityY += GRAVITY;
+        setY(getY() + velocityY);
+
+        if (getY() > groundY) {
+            setY(groundY);
+            velocityY = 0;
+            setImg(runImg);
+        }
+    }
+
+    public void jump() {
+        velocityY = JUMP_VELOCITY;
+        setImg(jumpImg);
+    }
+
+    public boolean isOnGround() {
+        return getY() == groundY;
+    }
+
+    public void die() {
+        setImg(deadImg);
+    }
+
+    public void reset() {
+        setY(groundY);
+        setImg(runImg);
+        velocityY = 0;
+    }
+
+    public boolean collidesWith(Block block) {
+        return getX() < block.getX() + block.getWidth() &&
+               getX() + getWidth() > block.getX() &&
+               getY() < block.getY() + block.getHeight() &&
+               getY() + getHeight() > block.getY();
+    }
+}
+
+class Cactus extends Block {
+    private static final int VELOCITY_X = -12;
+
+    public Cactus(int x, int y, int width, int height, Image img) {
+        super(x, y, width, height, img);
+    }
+
+    public void move() {
+        setX(getX() + VELOCITY_X);
+    }
+}
+
+class CactusFactory {
+    public static Cactus createCactus(double chance, int boardHeight) {
+        int cactusY = boardHeight - 70;
+        if (chance > 0.90) {
+            return new Cactus(700, cactusY, 102, 70, new ImageIcon(Cactus.class.getResource("./img/cactus3.png")).getImage());
+        } else if (chance > 0.70) {
+            return new Cactus(700, cactusY, 69, 70, new ImageIcon(Cactus.class.getResource("./img/cactus2.png")).getImage());
+        } else {
+            return new Cactus(700, cactusY, 34, 70, new ImageIcon(Cactus.class.getResource("./img/cactus1.png")).getImage());
+        }
+    }
+}
+
